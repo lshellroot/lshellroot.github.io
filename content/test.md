51145bf608c6764ba6b788880202bfae3efb3c7e
An Shen <shen.an89@gmail.com>
2015-05-14 17:24:23 +0800

    update blog

---
+++ content/test.md
@@ -0,0 +1,6 @@
+Title:Hello World
+Date:2015-05-14
+Category:Essay
+Tag:Essay
+Hello World,This is a linux shell script blog!
+by An.Shen
---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'An.Shen'
+SITENAME = u'Linux Shell'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Chongqing'
+
+DEFAULT_LANG = u'cn'
+THEME = "mg"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+DISQUS_SITENAME = u"linuxshell"
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+PLUGIN_PATHS = u"pelican-plugins"
+
+PLUGINS = ["sitemap"]
+
+## 配置sitemap 插件
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.7,
+        "indexes": 0.5,
+        "pages": 0.3,
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly",
+    }
+}
